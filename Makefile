8aabde1afa2575dd1fbca8b32782c530d99fdb83
Marton Bidlek <bidlekmarton@gmail.com>
2024-11-19 12:14:32 +0100

    making progress

--- Makefile
+++ Makefile
@@ -1,16 +1,16 @@
 all: project
 
 project: BMPImage.o GaussianBlur.o proj.o
-	nvcc -std=c++11 -g -o project BMPImage.o GaussianBlur.o proj.o
+	nvcc -std=c++14 -g -o project BMPImage.o GaussianBlur.o proj.o
 
 BMPImage.o: BMPImage.cpp
-	g++ -std=c++11 -O3 -g -c BMPImage.cpp -o BMPImage.o
+	g++ -std=c++14 -O3 -g -c BMPImage.cpp -o BMPImage.o
 
 GaussianBlur.o: GaussianBlur.cpp
-	g++ -std=c++11 -O3 -g -c GaussianBlur.cpp -o GaussianBlur.o
+	g++ -std=c++14 -O3 -g -c GaussianBlur.cpp -o GaussianBlur.o
 
 proj.o: proj.cu
-	nvcc -std=c++11 -g -c proj.cu -o proj.o
+	nvcc -std=c++14 -I /usr/local/include/eigen3 -g -c proj.cu -o proj.o
 
 clean:
 	rm -f BMPImage.o GaussianBlur.o proj.o project
--- proj.cu
+++ proj.cu
@@ -5,6 +5,7 @@
 #include <algorithm>
 #include "BMPImage.hpp"
 #include "GaussianBlur.hpp"
+#include <Eigen/Sparse>
 
 #define AssertCuda(error_code)                                           \
     if (error_code != cudaSuccess)                                       \
@@ -15,34 +16,76 @@
         std::abort();                                                    \
     }
 
+template <typename Number>
+void generateLMatrix(Eigen::SparseMatrix<Number> &L, int width, int height)
+{
+    int n = width * height;
+    L.resize(2 * n, n);
+    std::vector<Eigen::Triplet<Number>> tripletList;
+    for (int y = 0; y < height; ++y)
+    {
+        for (int x = 0; x < width; ++x)
+        {
+            int idx = y * width + x;
+            if (x < width - 1)
+            {
+                tripletList.emplace_back(idx, idx, -1);   
+                tripletList.emplace_back(idx, idx + 1, 1);
+            }
+        }
+    }
+    for (int y = 0; y < height; ++y)
+    {
+        for (int x = 0; x < width; ++x)
+        {
+            int idx = y * width + x;
+            int gradYRow = n + idx; 
+
+            if (y < height - 1)
+            {
+                tripletList.emplace_back(gradYRow, idx, -1);        
+                tripletList.emplace_back(gradYRow, idx + width, 1); 
+            }
+        }
+    }
+    L.setFromTriplets(tripletList.begin(), tripletList.end());
+}
 
 template <typename Number>
-void applyLaplacian(const unsigned char *input, std::vector<Number> &output, int width, int height)
+void printLMatrix(const Eigen::SparseMatrix<Number> &L)
 {
-    const int kernel[3][3] = {
-        {0, 1, 0},
-        {1, -4, 1},
-        {0, 1, 0}};
-
-    for (int y = 1; y < height - 1; ++y)
+    std::cout << "L: " << L.rows() << " x " << L.cols() << ")\n";
+    for (int k = 0; k < L.outerSize(); ++k)
     {
-        for (int x = 1; x < width - 1; ++x)
+        for (typename Eigen::SparseMatrix<Number>::InnerIterator it(L, k); it; ++it)
         {
-            Number sum = 0.0f;
-            for (int ky = -1; ky <= 1; ++ky)
-            {
-                for (int kx = -1; kx <= 1; ++kx)
-                {
-                    sum += input[(y + ky) * width + (x + kx)] * kernel[ky + 1][kx + 1];
-                }
-            }
-            output[y * width + x] = sum;
+            std::cout << "(" << it.row() << ", " << it.col() << ") = " << it.value() << "\n";
         }
     }
 }
 
 template <typename Number>
-__global__ void gradDescCuda(Number *x, const Number *b, const Number *laplacian, Number mu, Number learningRate, int width, int height, int maxIterations)
+__global__ void sparseMatVec(const Number *x, Number *dst,
+                          const Number *l_val, const int *l_col, const int *l_row,
+                          int size)
+{
+    int row = blockIdx.x * blockDim.x + threadIdx.x;
+    if (row < size)
+    {
+        Number sum = 0;
+        for (int i = l_row[row]; i < l_row[row + 1]; ++i)
+        {
+            sum += l_val[i] * x[l_col[i]];
+        }
+        dst[row] = sum;
+    }
+}
+
+template <typename Number>
+__global__ void gradDescCuda(Number *x, const Number *b,
+                             const Number *l_val, const int *l_col, const int *l_row,
+                             const Number *lx, Number mu, Number learningRate,
+                             int width, int height, int maxIterations)
 {
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
     int totalSize = width * height;
@@ -51,39 +94,76 @@ __global__ void gradDescCuda(Number *x, const Number *b, const Number *laplacian
     {
         for (int iter = 0; iter < maxIterations; ++iter)
         {
-            Number gradient = (x[idx] - b[idx]) + mu * laplacian[idx] * x[idx];
+            Number gradient = 2 * (x[idx] - b[idx]);
+
+            int startIdx = l_row[idx];
+            int endIdx = l_row[idx + 1];
+            for (int i = startIdx; i < endIdx; ++i)
+            {
+                int colIdx = l_col[i];
+                gradient += 2 * mu * l_val[i] * lx[colIdx];
+            }
+
             x[idx] -= learningRate * gradient;
         }
     }
 }
 
 template <typename Number>
-void solveMinCuda(std::vector<Number> &x, const std::vector<Number> &b, const std::vector<Number> &laplacian, Number mu, int width, int height)
+void solveMinCuda(std::vector<Number> &x, const std::vector<Number> &b, const Eigen::SparseMatrix<Number> &L, int width, int height)
 {
     int totalSize = width * height;
-    Number learningRate = 0.001f;
-    int maxIterations = 100;
     int blockSize = 256;
     int numBlocks = (totalSize + blockSize - 1) / blockSize;
 
 
+    
+    Number learningRate = static_cast<Number>(0.1f);
+    int maxIterations = 100;
+    Number mu = static_cast<Number>(5.0f);
+    
+
+
+    Number *d_x, *d_b;
     AssertCuda(cudaMalloc(&d_x, totalSize * sizeof(Number)));
     AssertCuda(cudaMalloc(&d_b, totalSize * sizeof(Number)));
-    AssertCuda(cudaMalloc(&d_laplacian, totalSize * sizeof(Number)));
 
     AssertCuda(cudaMemcpy(d_x, x.data(), totalSize * sizeof(Number), cudaMemcpyHostToDevice));
     AssertCuda(cudaMemcpy(d_b, b.data(), totalSize * sizeof(Number), cudaMemcpyHostToDevice));
-    AssertCuda(cudaMemcpy(d_laplacian, laplacian.data(), totalSize * sizeof(Number), cudaMemcpyHostToDevice));
 
-    gradDescCuda<<<numBlocks, blockSize>>>(d_x, d_b, d_laplacian, mu, learningRate, width, height, maxIterations);
+    Number *d_val;
+    int *d_col, *d_rowIdx;
+    int numNonZeros = L.nonZeros();
+
+    AssertCuda(cudaMalloc(&d_val, numNonZeros * sizeof(Number)));
+    AssertCuda(cudaMalloc(&d_col, numNonZeros * sizeof(int)));
+    AssertCuda(cudaMalloc(&d_rowIdx, (L.outerSize() + 1) * sizeof(int)));
+
+    AssertCuda(cudaMemcpy(d_val, L.valuePtr(), numNonZeros * sizeof(Number), cudaMemcpyHostToDevice));
+    AssertCuda(cudaMemcpy(d_col, L.innerIndexPtr(), numNonZeros * sizeof(int), cudaMemcpyHostToDevice));
+    AssertCuda(cudaMemcpy(d_rowIdx, L.outerIndexPtr(), (L.outerSize() + 1) * sizeof(int), cudaMemcpyHostToDevice));
+
+    Number *d_Lx;
+    AssertCuda(cudaMalloc(&d_Lx, 2 * totalSize * sizeof(Number)));
+
+    int numBlocks2 = (2 * totalSize + blockSize - 1) / blockSize;
+    sparseMatVec<<<numBlocks2, blockSize>>>(d_x, d_Lx, d_val, d_col, d_rowIdx, 2 * totalSize);
     AssertCuda(cudaGetLastError());
+
+    gradDescCuda<<<numBlocks, blockSize>>>(d_x, d_b, d_val, d_col, d_rowIdx, d_Lx, mu, learningRate, width, height, maxIterations);
+    AssertCuda(cudaGetLastError());
+
 
     AssertCuda(cudaMemcpy(x.data(), d_x, totalSize * sizeof(Number), cudaMemcpyDeviceToHost));
 
     AssertCuda(cudaFree(d_x));
     AssertCuda(cudaFree(d_b));
-    AssertCuda(cudaFree(d_laplacian));
+    AssertCuda(cudaFree(d_Lx));
+    AssertCuda(cudaFree(d_val));
+    AssertCuda(cudaFree(d_col));
+    AssertCuda(cudaFree(d_rowIdx));
 }
+
 
 int main()
 {
@@ -100,8 +180,11 @@ int main()
     BMPImage blurred(width, height, blurredImage.get());
     blurred.SaveBMP("lena_blurred.bmp");
 
-    std::vector<Number> laplacian(width * height, 0.0f);
-    applyLaplacian(blurredImage.get(), laplacian, width, height);
+
+    Eigen::SparseMatrix<Number> L;
+    generateLMatrix(L, width,height);
+    //printLMatrix(L);
+    
 
     std::vector<Number> b(width * height, 0.0f);
     for (int i = 0; i < width * height; ++i)
@@ -110,9 +193,8 @@ int main()
     }
 
     std::vector<Number> output(width * height, 0.0f);
-    Number mu = 1.0f;
 
-    solveMinCuda(output, b, laplacian, mu, width, height);
+    solveMinCuda(output, b, L, width, height);
 
     auto outputImage = std::unique_ptr<unsigned char[]>(new unsigned char[width * height]);
     Number minVal = *std::min_element(output.begin(), output.end());
